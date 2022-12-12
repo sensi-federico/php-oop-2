2ea0cfdb7d9a028e89dbfc538224ebdfb99e8db8
sensiz <dev.federicosensi@gmail.com>
2022-12-12 17:55:51 +0100

    add icons

--- index.php
+++ index.php
@@ -86,26 +86,30 @@ $cardProduct = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pet shop</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'>
 </head>
 
 <body class="bg-dark text-white">
 
     <h1 class="text-center py-5">Pet Shop</h1>
     <div class="container">
-        <div class="row row-cols-4 g-4">
+        <div class="row row-cols-3 g-4">
             <?php foreach ($cardProduct as $product) : ?>
-                <?php for ($i = 0; $i < 4; $i++) : ?>
-                    <div class="col">
-                        <div class="card text-black" style="width:18rem;">
-                            <img src="<?= $product->image ?>" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title text-capitalize"> <?= $product->name ?> </h5>
-                                <h6 class="card-subtitle mb-2 text-muted "> <?= $product->category->name ?> </h6>
-                                <h3 class="card-text text-black-50"><?= $product->getPrice() ?></h3>
-                            </div>
+                <div class="col">
+                    <div class="card text-black" style="width:18rem;">
+                        <img src="<?= $product->image ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-capitalize"> <?= $product->name ?> </h5>
+                            <?php if ($product->category->name == 'dog') : ?>
+
+                                <h6 class="card-subtitle mb-2 text-muted "> <i class="fa-solid fa-dog"></i> </h6>
+                            <?php else : ?>
+                                <h6 class="card-subtitle mb-2 text-muted "> <i class="fa-solid fa-cat"></i> </h6>
+                            <?php endif ?>
+                            <h3 class="card-text text-black-50"><?= $product->getPrice() ?></h3>
                         </div>
                     </div>
-                <?php endfor ?>
+                </div>
             <?php endforeach ?>
         </div>
     </div>
